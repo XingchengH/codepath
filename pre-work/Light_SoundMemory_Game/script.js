86d899e04d1958f59133da1167126c0ccfa2d30d
Xingcheng Huang <74392088+XingchengH@users.noreply.github.com>
2022-03-17 22:59:07 -0400

    Update script.js

--- pre-work/Light_SoundMemory_Game/script.js
+++ pre-work/Light_SoundMemory_Game/script.js
@@ -8,28 +8,23 @@ const nextClueWaitTime = 1000; //how long to wait before starting playback of th
 
 //Global variables
 //track the patterns of buttons
-
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
-// var pattern = [];
-// var arrMax = 10;
-// var limit = arrMax + 1;
-// for(var i = 0; i < arrMax; i++) {
-//   pattern.push(Math.floor(Math.random() * limit));
-// }
-//index
+var pattern = [];
+var patternLength = 10;
 var progress = 0;
-//check
 var gamePlaying = false;
 var tonePlaying = false;
-//track valume
 var volume = 0.5; //must between 0.0 - 1.0
 var guessCounter = 0;
+var numBtns = 4;
 
 function startGame() {
+  //generate random nums in the pattern
+  for(var i = 0; i < patternLength; i++) {
+    pattern[i] = Math.round(Math.random() * numBtns);
+  }
   //initialize game vars
   progress = 0;
   gamePlaying = true;
-
   //swap the start and stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
@@ -49,27 +44,29 @@ const freqMap = {
   3: 392,
   4: 466.2
 }
-function playTone(btn,len){
+
+function playTone(btn, len) {
   o.frequency.value = freqMap[btn]
-  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025)
   context.resume()
   tonePlaying = true
-  setTimeout(function(){
+  setTimeout(function() {
     stopTone()
-  },len)
+  }, len)
 }
 
-function startTone(btn){
-  if(!tonePlaying){
+function startTone(btn) {
+  if (!tonePlaying) {
     context.resume()
     o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025)
     context.resume()
     tonePlaying = true
   }
 }
-function stopTone(){
-  g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+
+function stopTone() {
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025)
   tonePlaying = false
 }
 
@@ -80,57 +77,57 @@ var context = new AudioContext()
 var o = context.createOscillator()
 var g = context.createGain()
 g.connect(context.destination)
-g.gain.setValueAtTime(0,context.currentTime)
+g.gain.setValueAtTime(0, context.currentTime)
 o.connect(g)
 o.start(0)
 
-function lightButton(btn){
-  document.getElementById("button"+btn).classList.add("lit")
+function lightButton(btn) {
+  document.getElementById("button" + btn).classList.add("lit")
 }
 
-function clearButton(btn){
-  document.getElementById("button"+btn).classList.remove("lit")
+function clearButton(btn) {
+  document.getElementById("button" + btn).classList.remove("lit")
 }
 
-function playSingleClue(btn){
-  if(gamePlaying){
+function playSingleClue(btn) {
+  if (gamePlaying) {
     lightButton(btn);
-    playTone(btn,clueHoldTime);
-    setTimeout(clearButton,clueHoldTime,btn);
+    playTone(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime, btn);
   }
 }
 
-function playClueSequence(){
+function playClueSequence() {
   guessCounter = 0;
   let delay = nextClueWaitTime; //set delay to initial wait time
-  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+  for (let i = 0; i <= progress; i++) { // for each clue that is revealed so far
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
-    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    setTimeout(playSingleClue, delay, pattern[i]) // set a timeout to play that clue
     delay += clueHoldTime
     delay += cluePauseTime;
   }
 }
 
-function loseGame(){
+function loseGame() {
   stopGame();
   alert("Game Over. You lost.");
 }
 
-function winGame(){
+function winGame() {
   stopGame();
   alert("Game Over. You won!");
 }
 
-function guess(btn){
+function guess(btn) {
   console.log("user guessed: " + btn);
-  if(!gamePlaying){
+  if (!gamePlaying) {
     return;
   }
 
   // add game logic here
-  if(pattern[guessCounter] == btn) {
-    if(guessCounter == progress) {
-      if(progress == pattern.length - 1) {
+  if (pattern[guessCounter] == btn) {
+    if (guessCounter == progress) {
+      if (progress == pattern.length - 1) {
         winGame();
       } else {
         progress++;
@@ -143,3 +140,4 @@ function guess(btn){
     loseGame();
   }
 }
+
